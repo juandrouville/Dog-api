45bc34e96ea891143e32efa64f5dfcee727edd69
juandrouville <juandrouville>
2022-06-09 16:35:24 -0300

    test app

--- src/db.js
+++ src/db.js
@@ -22,7 +22,7 @@ let sequelize = process.env.NODE_ENV === "production"
         dialectOptions:{
           ssl:{
             require: true,
-            // Ref.: https://github.com/brianc/node-postgres/issues/2009
+            
             rejectUnauthorized: false,
           },
           keepAlive:true,
@@ -37,26 +37,24 @@ const basename = path.basename(__filename);
 
 const modelDefiners = [];
 
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+ 
 fs.readdirSync(path.join(__dirname, '/models'))
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
   .forEach((file) => {
     modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
-// Injectamos la conexion (sequelize) a todos los modelos
+
 modelDefiners.forEach(model => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
+
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
+
 const { Dog, Temperament } = sequelize.models;
 
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+
 Dog.belongsToMany(Temperament,{through:"dog-temperament"});
 Temperament.belongsToMany(Dog,{through:"dog-temperament"});
 
