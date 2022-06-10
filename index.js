c0fa00868edcfd30b4d408a62af7490a8e58440b
juandrouville <juandrouville>
2022-06-10 18:14:05 -0300

    sync

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { conn } = require('./src/db.js');
 
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
+// conn.sync({ force: false }).then(() => {
   server.listen( process.env.PORT || 3001 , () => {
     console.log(process.env.PORT); // eslint-disable-line no-console
   });
-});
+// });
